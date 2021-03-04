0b2614f0487331ffa21a9ef191a815c5b7fd3bfd
Katelynn Cowan <kcowan@twu.edu>
2021-03-04 11:29:33 -0600

    Added new dependency const to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000
-
+const db = require("./models");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
